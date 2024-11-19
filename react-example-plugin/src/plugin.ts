795aa5e5be668e770be46457b757296df4647e2b
yassEmp <yassibello20@gmail.com>
2024-11-19 04:13:43 +0100

    fixed the height

--- react-example-plugin/src/plugin.ts
+++ react-example-plugin/src/plugin.ts
@@ -1,6 +1,6 @@
 import type { PluginMessageEvent } from './model';
 
-penpot.ui.open('React example plugin', `?theme=${penpot.theme}`, { width: 400, height: 700 });
+penpot.ui.open('React example plugin', `?theme=${penpot.theme}`, { width: 400, height: 800 });
 
 penpot.on('themechange', (theme) => {
   sendMessage({ type: 'theme', content: theme });
